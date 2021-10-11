b7b56f08028660aa6ad54174a973030458b962b9
Alexey2576 <alexeynikinitin@yandex.by>
2021-10-11 23:46:05 +0300

    lesson 3

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 let btn = document.getElementById('btn');
 let input = document.getElementById('input');
 
-$.ajax('https://repetitora.net/api/JS/Images', {
-  success: function (data) {
-    data.forEach(el => {
 
-      const img = document.createElement('img');
-      img.src = el.thumbnail;
-      document.querySelector('body').appendChild(img);
 
-    })
-  }
-});
+let clickk = () => {
+  $.ajax(`https://repetitora.net/api/JS/Images?page=${input.value}&count=1`, {
+    success: function (data) {
+      data.forEach(el => {
+        const img = document.createElement('img');
+        img.src = el.thumbnail;
+        document.querySelector('body').appendChild(img);
+      })
+    }
+  });
+}
 
-// btn.addEventListener("click", () => {console.log("sdsdsd")})
-
+btn.addEventListener("click", clickk);
